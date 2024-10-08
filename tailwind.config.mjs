051309b795bef8d945fc2e5a62da94b8e68411f8
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2024-10-08 11:58:07 +0700

    added animation for scroll

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+import { transformer } from 'astro/zod';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			keyframes: {
+				scroll: {
+					"100%": {
+						transform: "translate(calc(-50% - var(--gap) / 2))"
+					}
+				}
+			},
+			animation: {
+				scroll: "scroll var(--duration, 20s) linear var(--direction, forwards) infinite"
+			}
+		},
+	},
+	plugins: [],
+}
